d1a6bb6dab13d0d5494c8e1714416caea93cbe94
Hillking <976129707@qq.com>
2021-05-11 22:02:21 +0800

    shouye

--- admin-vue/src/views/Login.vue
+++ admin-vue/src/views/Login.vue
@@ -13,29 +13,31 @@
           <!--rules 属性传入约定的验证规则-->
           <!--prop 属性设置为需校验的字段名-->
           <!--设置表单: ref 绑定控件-->
-          <!--:model是v-bind:model的缩写,用于绑定数据-->
+          <!--:model是v-bind:model的缩写,用于绑定数据.v-model实现双向绑定数据-->
           <el-form :model="loginForm" :rules="rules"
-                   ref="loginForm" label-width="100px"
+                   ref="loginForm" label-width="25%"
                    class="demo-loginForm" label-position="left"
-                   size="mini" >
-
-            <el-form-item label="账号" prop="username">
-              <el-input v-model="loginForm.name" placeholder="请输入数字字母的组合"></el-input>
+                   size="mini">
+            <el-form-item label="账  号" class="label-font"  prop="username" >
+              <el-input v-model="loginForm.name" placeholder="Username" class="inputtext"></el-input>
             </el-form-item>
-            <el-form-item label="密码" prop="password">
-              <el-input v-model="loginForm.name" placeholder="请输入数字字母的组合"></el-input>
+            <el-form-item label="密  码"  class="label-font" prop="password">
+              <el-input  v-model="loginForm.name" placeholder="Password" class="inputtext"></el-input>
             </el-form-item>
-            <el-form-item label="验证码" prop="code">
-              <el-input v-model="loginForm.name" placeholder="点击图片刷新"></el-input>
+            <el-form-item label="验证码" class="label-font-code" prop="code" >
+              <el-input v-model="loginForm.name" placeholder="Check Code" class="inputcode inputtext"></el-input>
               <!--这个元素用于防止校验码图像,src是图像路径-->
-              <el-image src=""></el-image>
+              <el-image src="" class="codeimg"></el-image>
             </el-form-item>
 
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
-              <el-button @click="resetForm('loginForm')">重置</el-button>
-            </el-form-item>
+            <!--<el-form-item>-->
+            <!--  -->
+            <!--</el-form-item>-->
+            <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+            <el-button @click="resetForm('loginForm')">重置</el-button>
+
           </el-form>
+
         </div>
       </div>
       <div class="div-bottom-middle"></div>
@@ -96,63 +98,75 @@ export default {
   /*大小设置为100%*/
   width:100%;
   height:100%;
-  position:fixed;
+  position: fixed;
   background-size:100% 100%;
+  margin: -10px -10px -10px -10px;
+  /*display: flex;*/
+  /*align-items: center;*/
 }
 #div-title {
   width:100%;
-  height:25%;
-  position: sticky;
+  height:20%;
 }
 #title-font{
   color: white;
   text-align:center;
-  padding: 6% 2% 6% 2%;
+  padding: 3% 2% 9% 2%;
+  /*自适应调整字体大小*/
   font-size: 4vw;
 }
 .div-middle{
   width:100%;
-  height:50%;
-  position: sticky;
+  height:55%;
   display: flex; /*将子元素设置成一行: 并等分*/
 }
 .div-bottom{
   width:100%;
-  height:25%;
+  height:20%;
 }
 .div-bottom-middle{
-  position: sticky;
   flex: 1; /*将子元素设置成一行: 并等分*/
 }
 #form-bg {
-  height: 80%;
+  height: 100%;
   width: 100%;
-  background: #f9fafc;
-  opacity:0.5;
+  /*使用rgba设置半透明的背景色*/
+  background: rgba(248,248,255,0.5);
   border-radius: 6px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
 }
-
-
-
-
-
-#div-form {
-  padding: 0 30% 0 30%;
-
-}
-
-
-/*.el-form {*/
-/*  padding: 228px 180px;*/
-
-/*}*/
 #login-font{
-  font-size: larger;
-  position: relative;
-  z-index:1100;   /*设置图层不起作用*/
+  font-size: 2vw;
+  padding: 6% 2% 6% 2%;
   opacity:1
 }
+.el-form {
+  padding: 0 2% 6% 5%;
+}
+.label-font{
+  font-size: 1vw;
+  padding: -10px 0 0 0;
+}
+.label-font-code{
+  font-size: 1vw;
+}
+.inputtext{
+  float: left;
+  width: 80%;
+  height: 100%;
+  flex: 1; /*将子元素设置成一行: 并等分*/
+}
+.inputcode{
+  width: 55%;
+  height: 100%;
+  flex: 1;
+}
+.codeimg{
+  float: left;
+  margin-left: 8px;
+}
+
+
 
 </style>
 
