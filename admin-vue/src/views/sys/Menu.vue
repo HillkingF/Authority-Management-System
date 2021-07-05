f801e53bcbd9419b5af1be284b8cf67ef4f07970
Hillking <976129707@qq.com>
2021-07-05 22:01:50 +0800

    菜单页面

--- admin-vue/src/views/sys/Menu.vue
+++ admin-vue/src/views/sys/Menu.vue
@@ -1,10 +1,89 @@
 <template>
-  <div>菜单管理</div>
+  <div>
+    <!--行内表单部分-->
+    <el-form :inline="true">
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">新增</el-button>
+      </el-form-item>
+    </el-form>
+    <!--树形表单部分-->
+    <el-table
+        :data="tableData"
+        style="width: 100%;margin-bottom: 20px;"
+        row-key="id"
+        border
+        stripe
+        default-expand-all
+        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+      <!--el-table标签的属性说明
+       row-key与表单的每一行有关
+      :data表示从<script>中传来的数据
+      border表示有边框的
+      stripe表示行与行之间有斑马线的效果
+      -->
+      <el-table-column
+          prop="name"
+          label="名称"
+          sortable
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="perm"
+          label="权限编码"
+          sortable
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="icon"
+          label="图标">
+      </el-table-column>
+      <el-table-column
+          prop="type"
+          label="类型">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Menu"
+  name: "Menu",
+  data() {
+    return {
+      tableData: [{
+        id: 1,
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        id: 2,
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        id: 3,
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+        children: [{
+          id: 31,
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          id: 32,
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }]
+      }, {
+        id: 4,
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+    }
+  }
 }
 </script>
 
